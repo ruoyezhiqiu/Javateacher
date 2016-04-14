4084d6be7e790ec5ca831fe901e608a9c6f42fb7
thu <mingfei.net@gmail.com>
2016-04-14 13:45:55 +0800

    Update.

--- src/demo/CollectionTest.java
+++ src/demo/CollectionTest.java
@@ -1,9 +1,8 @@
 package demo;
 
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Created by teacher
@@ -11,17 +10,24 @@ import java.util.Map;
  */
 public class CollectionTest {
     public static void main(String[] args) {
-//        Iterable // 迭代
-//        Collection
-//        Map
+        List<String> arrayList = new ArrayList<>();
+        arrayList.add("hi");
+        arrayList.add("hello");
+        arrayList.add("hello");
 
-//        ArrayList<String> arrayList = new ArrayList<>();
-        List<String> stringList = new ArrayList<>();
-        stringList.add("hi");
-        stringList.add("hello");
-        stringList.add("hello");
+        arrayList.remove(2);
+        arrayList.set(1, "hi2");
 
-        System.out.println(stringList.get(0));
-        System.out.println(stringList.size());
+        System.out.println(arrayList.get(1));
+        System.out.println(arrayList.size());
+
+        List<String> linkedList = new LinkedList<>();
+        linkedList.add("hi");
+
+        linkedList.set(0, "hello");
+
+        System.out.println(linkedList.get(0));
+        linkedList.remove(0);
+        System.out.println(linkedList.size());
     }
 }
