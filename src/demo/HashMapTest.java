a1ed0ca5e536a68cf999648efab6d714d4fba148
thu <mingfei.net@gmail.com>
2016-04-14 15:19:03 +0800

    Update.

---
+++ src/demo/HashMapTest.java
@@ -0,0 +1,25 @@
+package demo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by teacher
+ * 15:16 2016/4/14.
+ */
+public class HashMapTest {
+    public static void main(String[] args) {
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "hi");
+        map.put(2, "hello");
+
+        System.out.println(map.size());
+
+        System.out.println(map.get(2));
+
+        for (Integer integer : map.keySet()) {
+            System.out.println(integer + " > " + map.get(integer));
+        }
+
+    }
+}
