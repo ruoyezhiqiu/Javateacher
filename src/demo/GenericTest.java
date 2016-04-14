5829a4d9a22e65443fc32d9d5c1564dfc14b8408
thu <mingfei.net@gmail.com>
2016-04-14 09:52:39 +0800

    Update.

--- src/demo/GenericTest.java
+++ src/demo/GenericTest.java
@@ -1,24 +1,30 @@
 package demo;
 
+import java.util.Vector;
+
 /**
  * Created by teacher
  * 9:12 2016/4/14.
  */
-public class GenericTest<T extends CharSequence> {
+public class GenericTest<T, S> {
 
-//    public static int add(int x, int y) {
-//        return x + y;
-//    }
-//
-//    public static double add(double x, double y) {
-//        return x + y;
-//    }
-
-    public T add(T x, T y) {
-        return x + y;
+    public String add(T x, S y) {
+        return String.valueOf(x) + String.valueOf(y);
     }
 
     public static void main(String[] args) {
+        GenericTest<Integer, Integer> genericTest = new GenericTest<>();
+        System.out.println(genericTest.add(1, 2));
 
+        GenericTest<Double, Double> doubleGenericTest = new GenericTest<>();
+        System.out.println(doubleGenericTest.add(1.1, 2.2));
+
+        GenericTest<Boolean, String> booleanStringGenericTest = new GenericTest<>();
+        System.out.println(booleanStringGenericTest.add(true, "hi"));
+
+        Vector<String> vector = new Vector<>();
+        vector.add("1");
+        String s = vector.get(0);
+        System.out.println(s);
     }
 }
